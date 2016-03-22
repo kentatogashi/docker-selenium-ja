0e0b71a42a0cb169dbcb83a180004f60dc51a22f
kentatogashi <kentatogashi@togashik>
2016-03-22 16:08:19 +0900

    addvnc port

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 export USER=root
 VNC_PORT=${VNC_PORT:-5901}
 SEL_PORT=${SEL_PORT:-4444}
+VNC_OPT="-rfbport ${VNC_PORT}"
 SEL_PORT_OPT="-port ${SEL_PORT}"
-vncserver :1 -geometry 1360x768 -depth 24
+vncserver :1 -geometry 1360x768 -depth 24 $VNC_OPT
 
 cd /home/selenium
 DISPLAY=:1 java -jar selenium-standalone.jar ${SEL_PORT_OPT} ${SEL_OPTS}
